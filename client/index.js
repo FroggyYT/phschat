69ce77e09b287774aab16bc34537250fb86ce26b
FroggyYT <44011185+FroggyYT@users.noreply.github.com>
2020-10-05 13:35:03 -0400

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,1 +1,34 @@
 var s = io();
+
+var loginScreen = document.createElement("div");
+var chatScreen = document.createElement("div");
+
+var messages = document.createElement("div");
+chatScreen.append(messages);
+
+var messageBox = document.createElement("input");
+chatScreen.append(messageBox);
+
+messageBox.onkeypress = k => {
+  if (k.keyCode != 13) return;
+  s.emit("message", messageBox.value)
+}
+
+var nameBox = document.createElement("input");
+nameBox.placeholder = "Username";
+loginScreen.append(nameBox);
+
+var loginButton = document.createElement("button");
+loginButton.textContent = "Login";
+loginScreen.append(loginButton);
+
+document.body.append(loginScreen);
+
+loginButton.addEventListener("click", () => {
+  s.emit("tryLogin", nameBox.value);
+});
+
+s.on("loginSuccess", d => {
+  loginScreen.remove();
+  document.body.append(chatScreen);
+});
