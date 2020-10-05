8fa04b122ebe1dc780b387e6c265663127170d9a
FroggyYT <44011185+FroggyYT@users.noreply.github.com>
2020-10-05 14:17:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,52 @@ server.listen(process.env.PORT || 80);
 var io = require("socket.io")(server,{});
 
 var SOCKETS = [];
+var NAMES = [];
 
 io.on("connection", (s) => {
+  SOCKETS.push(s.id);
+  NAMES.push(null);
   
+  s.on("tryLogin", d => {
+    var valid = true;
+    for (var i of NAMES) {
+      if (i == d) valid = false;
+    }
+    if (valid) {
+      s.emit("loginSuccess");
+      for (var i in SOCKETS) {
+        if (SOCKETS[i] == s.id) {
+          NAMES[i] = d; 
+        }
+      }
+      io.emit("join", d);
+    }
+  });
+  
+  s.on("message", d => {
+    var canMessage = false;
+    var name;
+    
+    for (var i in SOCKETS) {
+      if (SOCKETS[i] == s.id) {
+         if (NAMES[i] != null) canMessage = true;
+         name = NAMES[i];
+      }
+    }
+    
+    if (canMessage) io.emit("message", [name, d]);
+  });
+  
+  s.on("disconnect", d => {
+    for (var i in SOCKETS) {
+      if (SOCKETS[i] == s.id) {
+        if (NAMES[i] != null) {
+          io.emit("leave", NAMES[i]); 
+        }
+        
+        SOCKETS.splice(i, 1);
+        NAMES.splice(i, 1);
+      }
+    }
+  });
 });
