2caacac842858157e3a6332cfd541e5a8657f155
FroggyYT <44011185+FroggyYT@users.noreply.github.com>
2020-10-12 14:04:31 -0400

    Update index.js

--- client/index.js
+++ client/index.js
@@ -7,36 +7,6 @@ var messages = document.createElement("div");
 chatScreen.append(messages);
 chatScreen.style.position = "absolute";
 
-sizeEls();
-window.onresize = sizeEls;
-
-function sizeEls() {
-  var chatScreenWidth = innerWidth / 2;
-  var chatScreenLeft = (innerWidth / 2) - (innerWidth / 4)
-  
-  chatScreen.style.width = `${chatScreenWidth}px`;
-  chatScreen.style.left = `${chatScreenLeft}px`; 
-  
-  var messagesWidth = (chatScreenWidth * 3 / 4) - 1;
-  var messagesLeft = chatScreenLeft;
-  
-  messages.style.width = `${messagesWidth}px`;
-  messages.style.left = `${messagesLeft}`;
-  
-  var playerListWidth = (chatScreenWidth / 4) - 1;
-  var playerListLeft = chatScreenLeft + messagesWidth + 1;
-  
-  playerList.style.width = `${playerListWidth}px`;
-  playerList.style.left = `${playerListLeft}px`;
-  
-  var messageBoxWidth = messagesWidth;
-  var messageBoxLeft = messagesLeft;
-  
-  messageBox.style.width = `${messageBoxWidth}px`;
-  messageBox.style.top = "510px";
-  messageBox.style.left = `${messageBoxLeft}px`;
-}
-
 var messageBox = document.createElement("input");
 var messagesWidth = (innerWidth / 4) * 3 / 4;
 messageBox.placeholder = "Message";
@@ -70,6 +40,42 @@ messageBox.style.borderRadius = "8px";
 messageBox.style.padding = "8px";
 messageBox.style.position = "absolute";
 messageBox.style.left = "0px";
+
+
+
+
+sizeEls();
+window.onresize = sizeEls;
+
+function sizeEls() {
+  var chatScreenWidth = innerWidth / 2;
+  var chatScreenLeft = (innerWidth / 2) - (innerWidth / 4)
+  
+  chatScreen.style.width = `${chatScreenWidth}px`;
+  chatScreen.style.left = `${chatScreenLeft}px`; 
+  
+  var messagesWidth = (chatScreenWidth * 3 / 4) - 1;
+  var messagesLeft = chatScreenLeft;
+  
+  messages.style.width = `${messagesWidth}px`;
+  messages.style.left = `${messagesLeft}`;
+  
+  var playerListWidth = (chatScreenWidth / 4) - 1;
+  var playerListLeft = chatScreenLeft + messagesWidth + 1;
+  
+  playerList.style.width = `${playerListWidth}px`;
+  playerList.style.left = `${playerListLeft}px`;
+  
+  var messageBoxWidth = messagesWidth;
+  var messageBoxLeft = messagesLeft;
+  
+  messageBox.style.width = `${messageBoxWidth}px`;
+  messageBox.style.top = "510px";
+  messageBox.style.left = `${messageBoxLeft}px`;
+}
+
+
+
 
 s.on("resSocketList", d => {
   playerList.textContent = d.join(", ");
