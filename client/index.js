c62fa57b246d9365d1c92f97c7fd77bfa0bc3547
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 18:15:03 -0500

    Fix Empty Messages

--- client/index.js
+++ client/index.js
@@ -78,6 +78,7 @@ s.on("resSocketList", d => {
 
 messageBox.onkeypress = k => {
   if (k.keyCode != 13) return;
+  if (messageBox.value.length == 0) return;
   if (messageBox.value.charAt(0) == "/") {
     var str = messageBox.value.split("");
     str.splice(0, 1);
